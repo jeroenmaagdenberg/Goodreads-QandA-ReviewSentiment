7812bbdd811d009eaf98b51191ea0a646a0ffd9d
jeroenmaagdenberg <j.j.p.maagdenberg@tilburguniversity.edu>
2023-03-30 11:49:12 +0200

    playing around with coding

--- src/cleaning_gr_reviews.R
+++ src/cleaning_gr_reviews.R
@@ -4,11 +4,13 @@ library(stringr)
 library(readr)
 
 # Load the data frame of reviews
-df_reviews <- fread("~/Downloads/goodreads_reviews.csv")
+df_reviews <- fread("~/Downloads/goodreads_reviews.csv", nrows = 10000)
 # df_reviews <- fread("goodreads_reviews_clean.csv")
 
 reviews <- df_reviews %>%
   filter(review_text_incomplete != "")
+
+reviews$review_text_incomplete <- tolower(as.character(reviews$review_text_incomplete))
 
 # clean reviews
 reviews <- reviews %>%
@@ -16,12 +18,13 @@ reviews <- reviews %>%
     # remove links
     review_text_incomplete = str_remove_all(reviews$review_text_incomplete, "https\\S*"),
     review_text_incomplete = str_remove_all(reviews$review_text_incomplete, "http\\S*"),
-    review_text_incomplete = str_remove_all(reviews$review_text_incomplete, "goodreads.co*"),
-    # remove annoying html stuff
+    review_text_incomplete = str_remove_all(reviews$review_text_incomplete, "goodreads.com*"),
+    # remove html stuff
     review_text_incomplete = str_remove_all(reviews$review_text_incomplete, "amp"),
     review_text_incomplete = str_remove_all(reviews$review_text_incomplete, "&S*"),
     review_text_incomplete = str_remove_all(reviews$review_text_incomplete, "&#x27;|&quot;|&#x2F;"),
     review_text_incomplete = str_remove_all(reviews$review_text_incomplete, "<a(.*?)>"),
+    review_text_incomplete = str_remove_all(reviews$review_text_incomplete, "<a[^>]*>[^<]*</a>"),
     review_text_incomplete = str_remove_all(reviews$review_text_incomplete, "&gt;|&lt;|&amp;"),
     review_text_incomplete = str_remove_all(reviews$review_text_incomplete, "&#[:digit:]+;"),
     review_text_incomplete = str_remove_all(reviews$review_text_incomplete, "<[^>]*>"),
@@ -31,7 +34,6 @@ reviews <- reviews %>%
     review_text_incomplete = str_squish(review_text_incomplete),
     review_text_incomplete = str_trim(review_text_incomplete))
 
-reviews$review_text_incomplete <- tolower(as.character(reviews$review_text_incomplete))
 
 # write cleaned df to csv
 write.csv(reviews, "~/Documents/GitHub/Goodreads-QandA-ReviewSentiment/dat/goodreads_reviews_clean.csv", row.names = FALSE)
