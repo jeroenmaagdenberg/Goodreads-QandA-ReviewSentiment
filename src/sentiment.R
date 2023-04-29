772fd8933871af0471385204c0ae4ba5a65f491c
jeroenmaagdenberg <j.j.p.maagdenberg@tilburguniversity.edu>
2023-04-29 17:30:24 +0200

    removed sentiment AFINN per review

--- src/sentiment.R
+++ src/sentiment.R
@@ -89,38 +89,37 @@ book_sentiment_vader <- reviews %>%
 
 
 #####
-reviews_sentiment_afinn <-
-  tidy_afinn %>%
-  group_by(book_id, review_id) %>% 
-  summarise(sentiment_afinn = sum(value, na.rm = TRUE),
-            sentiment_afinn2 = mean(value, na.rm = TRUE)) %>%
-  mutate(
-    afinn = case_when(
-      sentiment_afinn > 0 ~ "positive",
-      sentiment_afinn < 0 ~ "negative",
-      TRUE ~ "neutral"
-    ),
-    afinn2 = case_when(
-      sentiment_afinn2 > 0 ~ "positive",
-      sentiment_afinn2 < 0 ~ "negative",
-      TRUE ~ "neutral"
-    )
-  )
-
+# reviews_sentiment_afinn <- tidy_afinn %>%
+#   group_by(book_id, review_id) %>% 
+#   summarise(sentiment_afinn = sum(value, na.rm = TRUE),
+#             sentiment_afinn2 = mean(value, na.rm = TRUE)) %>%
+#   mutate(
+#     afinn = case_when(
+#       sentiment_afinn > 0 ~ "positive",
+#       sentiment_afinn < 0 ~ "negative",
+#       TRUE ~ "neutral"
+#     ),
+#     afinn2 = case_when(
+#       sentiment_afinn2 > 0 ~ "positive",
+#       sentiment_afinn2 < 0 ~ "negative",
+#       TRUE ~ "neutral"
+#     )
+#   )
+# 
 ### accuracy 
-library(yardstick)
-conf_mat(reviews_sentiment_afinn,
-         afinn,
-         afinn2,
-         dnn = c("afinn_sum","afinn_mean")
-)
-
-accuracy(reviews_sentiment_afinn,
-         as.factor(afinn),
-         as.factor(afinn2)
-)
-
-
-### check for removed reviews due to the cleaning process
-# check <- full_join(reviews, reviews_sentiment_afinn, by = "review_id") %>% filter(is.na(sentiment_afinn))
-
+# library(yardstick)
+# conf_mat(reviews_sentiment_afinn,
+#          afinn,
+#          afinn2,
+#          dnn = c("afinn_sum","afinn_mean")
+# )
+# 
+# accuracy(reviews_sentiment_afinn,
+#          as.factor(afinn),
+#          as.factor(afinn2)
+# )
+# 
+# 
+# ### check for removed reviews due to the cleaning process
+# # check <- full_join(reviews, reviews_sentiment_afinn, by = "review_id") %>% filter(is.na(sentiment_afinn))
+# 
